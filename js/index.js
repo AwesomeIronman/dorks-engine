e2dc7dcf4f91c44b8b80d509146ad180b2949508
Cyrus Gracias <cyrusgracias4u@gmail.com>
2019-11-01 15:19:22 +0530

    Add google dorks usage

--- js/index.js
+++ js/index.js
@@ -3,7 +3,12 @@ var googleUrl = "https://www.google.com/search?q=";
 function searchGoogle() {
 
   var query = googleUrl + encodeURIComponent(
-    $("#pageTitle").val()
+    $("#searchText").val() +
+    " intitle:" + $("#pageTitle").val() +
+    " intext:" + $("#pageContent").val() +
+    " inurl:" + $("#pageUrl").val() +
+    " site:" + $("#siteUrl").val() +
+    " filetype:" + $("#pageFileType").val()
   );
 
   console.log(query);

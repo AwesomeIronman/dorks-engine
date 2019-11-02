a533102fb85d3e813f1f97bb242ed85ef5b84eae
Cyrus Gracias <cyrusgracias4u@gmail.com>
2019-11-02 22:53:19 +0530

    Cleaned up JS code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,34 @@ import "bootstrap";
 
 import "./../styles/main.scss";
 
-function searchGoogle () {
+$("#searchButton").click(searchGoogle);
+
+function searchGoogle() {
   var googleUrl = "https://www.google.com/search?q=";
 
+  // Form and encode URL to Google
   var query = googleUrl + encodeURIComponent(
     $("#searchText").val() +
-    " intitle:" + $("#pageTitle").val() +
-    " intext:" + $("#pageContent").val() +
-    " inurl:" + $("#pageUrl").val() +
-    " site:" + $("#siteUrl").val() +
-    " filetype:" + $("#pageFileType").val()
+    appendQuery("intitle", "pageTitle") +
+
+    appendQuery("intext", "pageContent") +
+
+    appendQuery("inurl", "pageUrl") +
+
+    appendQuery("site", "siteUrl") +
+
+    appendQuery("filetype", "pageFileType")
   );
 
   console.log(query);
-
+  // Open URL in new tab
   window.open(query, "_blank");
 }
 
-$("#searchButton").click(searchGoogle);
+// Get search/dork type and the form input element to retrieve user input and return full query string if user input exists
+function appendQuery($queryType, $inputElement) {
+  if ($("#" + $inputElement).val() !== "") {
+    return ` ${$queryType}:${$("#" + $inputElement).val()}`;
+  }
+  return "";
+}
